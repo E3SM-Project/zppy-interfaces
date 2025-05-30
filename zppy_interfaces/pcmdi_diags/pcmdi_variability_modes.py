d20db166d2c1d654d44184582930e121ce4500fa
Ryan Forsyth <forsyth2@llnl.gov>
2025-05-29 22:56:32 -0500

    Refactor and fixes

--- zppy_interfaces/pcmdi_diags/pcmdi_variability_modes.py
+++ zppy_interfaces/pcmdi_diags/pcmdi_variability_modes.py
@@ -1,26 +1,143 @@
 import argparse
+import glob
+import os
 import sys
 import time
+from collections import OrderedDict
 from typing import Dict, List
 
-from pcmdi_zppy_util import (
-    VariabilityMetricsCollector,
-    generate_varmode_cmds,
-    run_parallel_jobs,
-    run_serial_jobs,
-)
-
-from zppy_interfaces.pcmdi_diags.pcmdi_common import CoreOutput, CoreParameters, set_up
+from zppy_interfaces.pcmdi_diags.pcmdi_setup import CoreOutput, CoreParameters, set_up
+from zppy_interfaces.pcmdi_diags.utils import run_parallel_jobs, run_serial_jobs
 
 
+# Classes #####################################################################
 class VariabilityModesParameters(object):
     def __init__(self, args: Dict[str, str]):
         self.var_modes: List[str] = args["var_modes"].split(",")
         # self.vars is distinct from the list version in CoreParameters
         self.vars: str = args["vars"]
-        self.figure_format: str = args["figure_format"]
 
 
+class VariabilityMetricsCollector:
+    def __init__(
+        self, modes, fig_format, mip, exp, model, relm, case_id, input_dir, output_dir
+    ):
+        self.modes = modes
+        self.fig_format = fig_format
+        self.mip = mip
+        self.exp = exp
+        self.model = model
+        self.relm = relm
+        self.case_id = case_id
+        self.input_dir = input_dir.replace("%(metric_type)", "variability_modes")
+        self.output_dir = output_dir
+        self.model_name = f"{mip}.{exp}.{model}_{relm}"
+        self.seasons = ["DJF", "MAM", "JJA", "SON", "yearly", "monthly"]
+        self.fig_sets = OrderedDict(
+            {
+                "MOV_eoftest": ["diagnostic_results", "EG_Spec*"],
+                "MOV_compose": ["graphics", "*compare_obs"],
+                "MOV_telecon": ["graphics", "*teleconnection"],
+                "MOV_pattern": ["graphics", "*"],
+            }
+        )
+
+    def collect(self):
+        self._collect_figures()
+        self._collect_metrics()
+        self._collect_diags()
+
+    def _collect_figures(self):
+        for fig_set, (out_type, pattern_base) in self.fig_sets.items():
+            for mode in self.modes:
+                for season in self.seasons:
+                    indir = self.input_dir.replace("%(output_type)", out_type)
+                    template = (
+                        f"{pattern_base}_{mode}_{season}*.{self.fig_format}"
+                        if fig_set == "MOV_eoftest"
+                        else f"{mode}_*_{season}_{pattern_base}.{self.fig_format}"
+                    )
+                    search_path = os.path.join(indir, mode, "*", template)
+                    matched_files = sorted(glob.glob(search_path))
+
+                    for fpath in matched_files:
+                        filename = os.path.basename(fpath)
+                        outfile = self._classify_output_name(
+                            fig_set, mode, season, filename
+                        )
+                        outdir = os.path.join(
+                            self.output_dir.replace("%(group_type)", "MOV_metric"),
+                            fig_set,
+                            season,
+                        )
+                        os.makedirs(outdir, exist_ok=True)
+                        os.rename(fpath, os.path.join(outdir, outfile))
+
+    def _classify_output_name(self, fig_set, mode, season, filename):
+        suffix = "unknown"
+        if "North_test" in filename:
+            suffix = "EG_Spec"
+        elif "_cbf_" in filename:
+            suffix = "cbf"
+        elif "EOF1" in filename:
+            suffix = "eof1"
+        elif "EOF2" in filename:
+            suffix = "eof2"
+        elif "EOF3" in filename:
+            suffix = "eof3"
+        return f"{fig_set}_{mode}_{season}_{suffix}.{self.fig_format}"
+
+    def _collect_metrics(self):
+        metrics_dir = self.input_dir.replace("%(output_type)", "metrics_results")
+        json_files = sorted(glob.glob(os.path.join(metrics_dir, "*/*/*.json")))
+
+        for fpath in json_files:
+            refmode = fpath.split("/")[-3]
+            refname = fpath.split("/")[-2]
+            reffile = fpath.split("/")[-1]
+
+            eof_lookup = {"PSA1": "EOF2", "NPO": "EOF2", "NPGO": "EOF2", "PSA2": "EOF3"}
+            refeof = eof_lookup.get(refmode, "EOF1")
+
+            outdir = os.path.join(
+                self.output_dir.replace("%(group_type)", "metrics_data"),
+                "variability_modes",
+                refmode,
+                refname,
+            )
+            os.makedirs(outdir, exist_ok=True)
+
+            base_name = f"var_mode_{refmode}.{refeof}.{self.model_name}.vs.{refname}.{self.case_id}"
+            if "diveDown" in reffile:
+                outfile = os.path.join(outdir, f"{base_name}.diveDown.json")
+            else:
+                outfile = os.path.join(outdir, f"{base_name}.json")
+
+            os.rename(fpath, outfile)
+
+    def _collect_diags(self):
+        diags_dir = self.input_dir.replace("%(output_type)", "diagnostic_results")
+        json_files = sorted(glob.glob(os.path.join(diags_dir, "*/*/*.nc")))
+
+        for fpath in json_files:
+            refmode = fpath.split("/")[-3]
+            refname = fpath.split("/")[-2]
+            reffile = fpath.split("/")[-1]
+
+            outdir = os.path.join(
+                self.output_dir.replace("%(group_type)", "metrics_data"),
+                "variability_modes",
+                refmode,
+                refname,
+            )
+            os.makedirs(outdir, exist_ok=True)
+
+            outfile = os.path.join(outdir, reffile)
+
+            os.rename(fpath, outfile)
+
+
+# Functions ###################################################################
 def main():
     args: Dict[str, str] = _get_args()
     core_parameters = CoreParameters(args)
@@ -32,6 +149,10 @@ def main():
     ##########################################
     # from configuration file
     varOBS = variability_modes_parameters.vars
+    if varOBS not in core_output.obs_dic:
+        raise KeyError(
+            f"VarOBS '{varOBS}' not found in obs_dic. Available keys are {core_output.obs_dic.keys()}"
+        )
     refset = core_output.obs_dic[varOBS]["set"]
     refname = core_output.obs_dic[varOBS][refset]
     refpath = core_output.obs_dic[varOBS][refname]["file_path"]
@@ -76,7 +197,7 @@ def main():
     split_name: List[str] = core_parameters.model_name.split(".")
     collector = VariabilityMetricsCollector(
         modes=variability_modes_parameters.var_modes,
-        fig_format=variability_modes_parameters.figure_format,
+        fig_format=core_parameters.figure_format,
         mip=split_name[0],
         exp=split_name[1],
         model=split_name[2],
@@ -102,6 +223,8 @@ def _get_args() -> Dict[str, str]:
     parser.add_argument("--climo_ts_dir_primary", type=str)  # needs ts_dir_primary
     parser.add_argument("--climo_ts_dir_ref", type=str)  # needs ts_dir_ref
     parser.add_argument("--model_name", type=str)
+    parser.add_argument("--model_tableID", type=str)
+    parser.add_argument("--figure_format", type=str)
     parser.add_argument("--run_type", type=str)
     parser.add_argument("--obs_sets", type=str)  # run_type == "model_vs_obs" only
     parser.add_argument(
@@ -115,10 +238,38 @@ def _get_args() -> Dict[str, str]:
 
     # For VariabilityModesParameters
     parser.add_argument("--var_modes", type=str)  # use either atm_mdoes or cpl_modes
-    parser.add_argument("--figure_format", type=str)
 
     # Ignore the first arg
     # (zi-pcmdi-variability-modes)
     args: argparse.Namespace = parser.parse_args(sys.argv[1:])
 
     return vars(args)
+
+
+def generate_varmode_cmds(modes, varOBS, reftyrs, reftyre, refname, refpath, case_id):
+    """Generates a list of command strings for variability modes processing."""
+
+    # EOF mode overrides for specific variability modes (default is 1)
+    eofn_map = {"NPO": 2, "NPGO": 2, "PSA1": 2, "PSA2": 3}
+
+    commands = []
+
+    for var_mode in modes:
+        var_mode = var_mode.strip()
+        # Use specified EOF number if in map, otherwise default to 1
+        eofn = eofn_map.get(var_mode, 1)
+        cmd = (
+            f"variability_modes_driver.py -p parameterfile.py "
+            f"--variability_mode {var_mode} "
+            f"--eofn_mod {eofn} "
+            f"--eofn_obs {eofn} "
+            f"--varOBS {varOBS} "
+            f"--osyear {reftyrs} "
+            f"--oeyear {reftyre} "
+            f"--reference_data_name {refname} "
+            f"--reference_data_path {refpath} "
+            f"--case_id {case_id}"
+        )
+        commands.append(cmd)
+
+    return commands
---
+++ zppy_interfaces/pcmdi_diags/utils.py
@@ -0,0 +1,92 @@
+import time
+from subprocess import PIPE, Popen
+from typing import List, Tuple
+
+import psutil
+
+from zppy_interfaces.multi_utils.logger import _setup_custom_logger
+
+logger = _setup_custom_logger(__name__)
+
+
+def count_child_processes(process=None):
+    """
+    Count the number of child processes for a given process.
+
+    Parameters:
+    - process (psutil.Process, optional): The process to check. If None, uses the current process.
+
+    Returns:
+    - int: Number of child processes.
+    """
+    if process is None:
+        process = psutil.Process()
+
+    children = process.children()
+    return len(children)
+
+
+def run_parallel_jobs(cmds: List[str], num_workers: int) -> List[Tuple[str, str, int]]:
+    """
+    Execute shell commands in parallel batches.
+
+    Parameters:
+    - cmds: List of command strings to run.
+    - num_workers: Maximum number of subprocesses to run concurrently.
+
+    Returns:
+    - List of tuples: (stdout, stderr, return_code) for each command.
+    """
+    results = []
+    procs = []
+
+    for i, cmd in enumerate(cmds):
+        proc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True, text=True)
+        procs.append((cmd, proc))
+
+        # Run the batch if full or if it's the last command
+        if len(procs) >= num_workers or i == len(cmds) - 1:
+            logger.info(f"Running {count_child_processes()} subprocesses...")
+            for cmd, proc in procs:
+                stdout, stderr = proc.communicate()
+                return_code = proc.returncode
+
+                if return_code != 0:
+                    logger.error(
+                        f"ERROR: Process failed: '{cmd}'\nError: {stderr.strip()}"
+                    )
+                    raise RuntimeError(f"Subprocess failed: {cmd}")
+
+                results.append((stdout.strip(), stderr.strip(), return_code))
+
+            time.sleep(0.25)  # Throttle before starting the next batch
+            procs = []
+
+    return results
+
+
+def run_serial_jobs(cmds: List[str]) -> List[Tuple[str, str, int]]:
+    """
+    Execute shell commands one at a time (serially).
+
+    Parameters:
+    - cmds: List of command strings to run.
+
+    Returns:
+    - List of tuples: (stdout, stderr, return_code) for each command.
+    """
+    results = []
+
+    for i, cmd in enumerate(cmds):
+        logger.info(f"Running [{i+1}/{len(cmds)}]: {cmd}")
+        proc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True, text=True)
+        stdout, stderr = proc.communicate()
+        return_code = proc.returncode
+
+        if return_code != 0:
+            logger.error(f"ERROR: Process failed: '{cmd}'\nError: {stderr.strip()}")
+            raise RuntimeError(f"Subprocess failed: {cmd}")
+
+        results.append((stdout.strip(), stderr.strip(), return_code))
+
+    return results
